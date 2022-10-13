becdfad81539102539654b2cbaec6be6898b5c25
anasgfdi <anas.gfdi@yahoo.fr>
2022-10-13 09:23:22 +0200

    Creation de PersonnalRemedies

---
+++ PersonnalRemedies/PersonnalRemedies/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-z0+=0(!03@ab&g#%+_xw9&c8s5t2*%*dpktz6$onv40)=g3fj&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
